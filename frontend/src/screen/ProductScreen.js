38f0a92c1dbc37a343c9e88de6c1ee645ecc7542
abhishekkmrpathak <abhishek.pathak2515@gmail.com>
2021-04-20 11:38:01 +0530

    new

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,14 +1,54 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { Row,Col,Image,ListGroup,Card,Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
+import {Rating}from '../components/Rating'
 import products from '../products'
 
 function ProductScreen({match}) {
-    const product = products.find((p) =>  p._id = match.params.id)
+    const product = products.find((p) =>  p._id == match.params.id)
     return (
         <div>
-           {product.name}
+           <Link to = '/' className = 'btn btn-light my-3'> GO BACK </Link>
+           <Row>
+               <Col md ={6}>
+                   <Image src={product.image} alt={product.name} fluid/>
+               </Col>
+               <Col md={3}>
+                   <ListGroup variant = "flush">
+                       <ListGroup.Item>
+                           <h3>{product.name}</h3>
+                       </ListGroup.Item>
+
+                       <ListGroup.Item>
+                           Price : {product.price}
+                       </ListGroup.Item>
+
+                       <ListGroup.Item>
+                            Rating : {product.rating} from {product.numReviews} reviews
+                       </ListGroup.Item>
+                       
+                        <ListGroup.Item>
+                                          Description:  {product.description}
+                        </ListGroup.Item>
+                   </ListGroup>
+               </Col>
+               <Col md ={3}>
+                   <Card>
+                   <ListGroup variant='flush'>
+                       <ListGroup.Item>
+                           <Row>
+                              <strong> <Col>Price : {product.price}</Col></strong>
+                           </Row>
+                       </ListGroup.Item>
+                       <ListGroup.Item>
+                           <Row>
+                              <strong> <Col>Status: {product.countInStock} in STOCK</Col></strong>
+                           </Row>
+                       </ListGroup.Item>
+                   </ListGroup>
+                   </Card>
+               </Col>
+           </Row>
         </div>
     )
 }
