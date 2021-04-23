29af54dd88ed3dffd0b67c587263646c197e6a71
abhishekkmrpathak <abhishek.pathak2515@gmail.com>
2021-04-23 12:57:56 +0530

    change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,7 @@ from django.urls import path , include
 
 urlpatterns = [
   
-    path('/', include('base.urls')),
+    path('admin/', admin.site.urls),
+    path(('api/'), include('base.urls')),
+
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http  import JsonResponse
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello THere',safe=False)
+    routes = [
+    'api/products/',
+    'api/products/create/',
+    'api/products/upload',
+    ]
+    return Response('Hello THere',safe=False)  
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product = None
+    for i in products:
+        if i['_id']==pk:
+             product= i 
+             break
+    return Response(product)
+  
