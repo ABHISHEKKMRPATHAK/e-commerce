43eb6f368d1238ebe52fef3969083d3e332b8e77
abhishekkmrpathak <abhishek.pathak2515@gmail.com>
2021-04-25 13:22:12 +0530

    new

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Products
+from .models import *
+
 # Register your models here.
 admin.site.register(Products)
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(Review)
--- backend/base/models.py
+++ backend/base/models.py
@@ -17,4 +17,70 @@ class Products(models.Model):
 
     def __str__(self):
          return self.name
-      
+    
+class User(models.Model):
+    _id = models.IntegerField(primary_key=True,null=False,blank=False) 
+    userName = models.CharField(max_length=255,null=False,blank=False) 
+    lastName =  models.CharField(max_length=50,null=False,blank=False)
+    email = models.CharField(max_length=255,null=False,blank=False)
+    password = models.CharField(max_length=255,null=False,blank=False)
+    isStaff = models.BooleanField(null=False)
+    isActive = models.BooleanField(null=False)
+    isSuperuser = models.BooleanField(null=False)
+   
+    def __str__(self):
+        return self.User
+    
+class Review(models.Model):
+    _id =  models.IntegerField(primary_key=True,null=False,blank=False)
+    user = models.IntegerField(null=True,blank=False)
+    product = models.IntegerField(null=True,blank=False)
+    name = models.CharField(max_length=255,null=True,blank=False)
+    rating= models.IntegerField(null=False,blank=False)
+    comment = models.TextField(null=False,blank=False) 
+    createdAt = models.DateTimeField(null=False,blank=False) 
+
+    def __str__(self):
+        return self.Review
+    
+
+class Order(models.Model):
+    _id=models.IntegerField(primary_key=True,null=False,blank=False)
+    user= models.IntegerField(null=False,blank=False)
+    paymentMethod = models.CharField(max_length=255,null=False,blank=False)
+    taxprice = models.DecimalField(max_digits=7,decimal_places=2,null=False,blank=False)
+    shippingPrice = models.DecimalField(max_digits=7,decimal_places=2,null=False,blank=False)
+    totalPrice = models.DecimalField(max_digits=7,decimal_places=2,null=False,blank=False)
+    isPaid = models.BooleanField(null=False,blank=False)
+    paidAt = models.IntegerField(null=False,blank=False)
+    isDelivered= models.IntegerField(null=False,blank=False)
+    deliveredAt = models.DateTimeField(null=False,blank=False)
+    createdAt=  models.DateTimeField(null=False,blank=False)
+
+    def __str__(self):
+        return self.Order
+    
+
+class ShippingAddress(models.Model):
+    _id = models.IntegerField(primary_key=True,null=False,blank=False) 
+    order = models.IntegerField(null=False,blank=False)
+    address = models.CharField(max_length=255,null= False,blank=False) 
+    city = models.CharField(max_length=255 , null= False,blank=False)
+    pinCode = models.IntegerField(null=False,blank=False)
+    country = models.CharField(max_length=255,null=False,blank=False)
+    shippingprice =models.DecimalField( max_digits=7, decimal_places=2,null=False,blank=False)
+
+    def __str__(self):
+        return self.ShippingAddress
+    
+
+class OrderItem(models.Model):
+    _id =models.IntegerField(primary_key=True,null=False,blank=False)
+    order = models.IntegerField(null=False,blank=False)
+    product =models.IntegerField(null=False,blank=False)
+    name = models.CharField(max_length=255,null=False,blank=False)
+    quantity = models.IntegerField(null=False,blank=False)
+
+    def __str__(self):
+        return self.OrderItem
+    
